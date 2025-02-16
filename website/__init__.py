07b7e190344de58d9425efa0756588de79fb2157
Shreyansh <shreyansh1114@gmail.com>
2025-02-17 00:19:27 +0530

    commit 3

--- website/__init__.py
+++ website/__init__.py
@@ -16,11 +16,17 @@ def create_app():
     Application factory to create and configure the Flask app instance.
     """
     app = Flask(__name__)
+<<<<<<< HEAD
+=======
+    app.debug = True
+    app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+>>>>>>> 3719b3d81093853cd794739f61999b6ef6a4da2f
 
     # Basic configuration
     app.debug = True
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-secret-key')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+<<<<<<< HEAD
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=100)  # Sessions expire after 30 minutes
 
@@ -29,14 +35,34 @@ def create_app():
         os.makedirs('instance')
 
     # Initialize extensions
+=======
+>>>>>>> 3719b3d81093853cd794739f61999b6ef6a4da2f
     db.init_app(app)
 
     # Import and register blueprints
     from .views import views
     from .auth import auth
+<<<<<<< HEAD
     from .add_event import add_event
     from .add_contact import add_contact
     from.team import Teams
+=======
+
+    from .models import Admin
+    from .models import Event
+    from .models import Team
+    from .models import Match
+
+    # without this login won't be possible
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = "Please log-in if you're admin"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Admin.query.get(int(id))
+>>>>>>> 3719b3d81093853cd794739f61999b6ef6a4da2f
 
     app.register_blueprint(views)
     app.register_blueprint(auth, url_prefix='/auth')
